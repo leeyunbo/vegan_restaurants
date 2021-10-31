9b214c6a17e20fa860bd0fbdeb7419b640ab2b51
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2021-10-31 21:10:25 +0900

    [design] 구현과 상관 없는 소스 위치 수정

--- src/main/java/com/vegan/restaurant/service/RestaurantService.java
+++ src/main/java/com/vegan/restaurant/service/RestaurantService.java
@@ -7,13 +7,10 @@ import com.vegan.restaurant.repository.CoordinateRepository;
 import com.vegan.restaurant.repository.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
